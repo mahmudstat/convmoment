114776abf3a22bb041b0dda416fc4c4be78ece61
mahmudstat <almahmud.sbi@gmail.com>
2024-09-16 07:34:23 +0600

    Add na_rm param

--- NEWS.md
+++ NEWS.md
@@ -1,8 +1,12 @@
 # convmoment (development version)
 
+* Added `na_rm` param
+
+# convmoment 0.2.1.9000
+
 * Added the function `get_moments()`, which can calculate moments up to `rth`
   degree. 
-* Added package-level documentation.   
+* Added package-level documentation.  
 
 # convmoment 0.0.1.9000
 
--- R/get_moments.R
+++ R/get_moments.R
@@ -9,6 +9,7 @@
 #' @param x A numeric vector with values of a variable
 #' @param a The origin
 #' @param r The highest order
+#' @param na_rm whether to remove NA; The default value is FALSE
 #' @param decimal The no. of digits after decimal
 
 #' @returns A data frame of `r` rows and 2 columns; the columns represent
@@ -19,11 +20,11 @@
 #' get_moments(x, 2, 3) # test r > 0
 
 #' @export
-get_moments <- function(x, a, r, decimal = 3){
+get_moments <- function(x, a, r, na_rm = FALSE, decimal = 3){
   options(scipen=999)
   mu <- c()
   for (i in 1:r) {
-    mu[i] <- sum((x-a)^i)/length(x)
+    mu[i] <- sum((x-a)^i, na.rm = na_rm)/length(x)
     }
   df <- data.frame(Order = 1:r,
                    Moment = round(mu, decimal))
